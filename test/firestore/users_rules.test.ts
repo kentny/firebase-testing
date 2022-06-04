67f9df3973dcae61b432b520281cbedc594ca759
Kentaro Terasaki <kterasaki@vmware.com>
2022-06-04 18:38:10 +0900

    Add firestore users/{userId}/tweets/{tweetId} security rule code and test.

--- test/firestore/users_rules.test.ts
+++ test/firestore/users_rules.test.ts
@@ -3,8 +3,9 @@ import * as testing from '@firebase/rules-unit-testing';
 
 const projectId = 'test-project';
 const testUserId = 'Test-User';
+const testUserTweetId = 'Test-User-Tweet';
 
-describe('Testing users (users/{userId}) security rule', () => {
+describe('Testing users (users/**) security rule', () => {
     let testEnv: testing.RulesTestEnvironment;
     let authenticatedUser:  testing.RulesTestContext;
     let unauthenticatedUser:  testing.RulesTestContext;
@@ -20,74 +21,170 @@ describe('Testing users (users/{userId}) security rule', () => {
         });
     });
 
-    beforeEach(async () => {
-        // Add initial data.
-        await testEnv.withSecurityRulesDisabled(context => {
-            const firestoreWithoutRule = context.firestore()
-            return firestoreWithoutRule
+    describe('Testing users (users/{userId}) security rule', () => {
+        beforeEach(async () => {
+            // Add initial data.
+            await testEnv.withSecurityRulesDisabled(context => {
+                const firestoreWithoutRule = context.firestore()
+                return firestoreWithoutRule
+                    .collection('users')
+                    .doc(testUserId)
+                    .set({ name: 'initial user name' })
+            })
+            authenticatedUser = testEnv.authenticatedContext(testUserId);
+            unauthenticatedUser = testEnv.unauthenticatedContext();
+        })
+
+        it('Unauthenticated user can READ.', async () => {
+            const readUser = unauthenticatedUser.firestore()
                 .collection('users')
                 .doc(testUserId)
-                .set({ name: 'initial user name' })
+                .get()
+
+            await testing.assertSucceeds(readUser);
+        });
+
+        it('Only authenticated user can CREATE.', async () => {
+            const createByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .add({ name: "authenticated user name" })
+
+            await testing.assertSucceeds(createByAuthenticatedUser);
+
+
+            const createByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .add({ name: "unauthenticated user name" })
+
+            await testing.assertFails(createByUnauthenticatedUser);
+        });
+
+        it('Only authenticated user can UPDATE.', async () => {
+            const updateByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .update({ name: "authenticated user name" })
+
+            await testing.assertSucceeds(updateByAuthenticatedUser);
+
+
+            const updateByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .update({ name: "authenticated user name" })
+
+            await testing.assertFails(updateByUnauthenticatedUser);
+        });
+
+        it('Nobody can DELETE.', async () => {
+            const deleteByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .delete()
+
+            await testing.assertFails(deleteByAuthenticatedUser);
+
+
+            const deleteByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .delete()
+
+            await testing.assertFails(deleteByUnauthenticatedUser);
+        });
+    });
+
+
+    describe('Testing tweets under users (users/{userId}/tweets/{tweetId}) security rule', () => {
+        beforeEach(async () => {
+            // Add initial data.
+            await testEnv.withSecurityRulesDisabled(async context => {
+                const firestoreWithoutRule = context.firestore()
+                const testUserRef = firestoreWithoutRule
+                    .collection('users')
+                    .doc(testUserId)
+
+                await testUserRef.set({ name: 'initial user name' })
+                await testUserRef
+                    .collection('tweets')
+                    .doc(testUserTweetId)
+                    .set({ text: 'hello, this is my tweet.' })
+
+                authenticatedUser = testEnv.authenticatedContext(testUserId);
+                unauthenticatedUser = testEnv.unauthenticatedContext();
+            })
         })
-        authenticatedUser = testEnv.authenticatedContext(testUserId);
-        unauthenticatedUser = testEnv.unauthenticatedContext();
-    })
 
-    it('Unauthenticated user can READ.', async () => {
-        const readUser = unauthenticatedUser.firestore()
-            .collection('users')
-            .doc('Test-User')
-            .get()
+        it('Unauthenticated user can READ.', async () => {
+            const readUserTweet = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .doc(testUserTweetId)
+                .get()
 
-        await testing.assertSucceeds(readUser);
-    });
+            await testing.assertSucceeds(readUserTweet);
+        });
 
-    it('Only authenticated user can CREATE.', async () => {
-        const createByAuthenticatedUser = authenticatedUser.firestore()
-            .collection('users')
-            .add({ name: "authenticated user name" })
+        it('Only authenticated user can CREATE.', async () => {
+            const createByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .add({ text: "authenticated user tweet" })
 
-        await testing.assertSucceeds(createByAuthenticatedUser);
-
-
-        const createByUnauthenticatedUser = unauthenticatedUser.firestore()
-            .collection('users')
-            .add({ name: "unauthenticated user name" })
-
-        await testing.assertFails(createByUnauthenticatedUser);
-    });
-
-    it('Only authenticated user can UPDATE.', async () => {
-        const updateByAuthenticatedUser = authenticatedUser.firestore()
-            .collection('users')
-            .doc(testUserId)
-            .update({ name: "authenticated user name" })
-
-        await testing.assertSucceeds(updateByAuthenticatedUser);
+            await testing.assertSucceeds(createByAuthenticatedUser);
 
 
-        const updateByUnauthenticatedUser = unauthenticatedUser.firestore()
-            .collection('users')
-            .doc(testUserId)
-            .update({ name: "authenticated user name" })
+            const createByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .add({ text: "authenticated user tweet" })
 
-        await testing.assertFails(updateByUnauthenticatedUser);
-    });
+            await testing.assertFails(createByUnauthenticatedUser);
+        });
 
-    it('Nobody can DELETE.', async () => {
-        const deleteByAuthenticatedUser = authenticatedUser.firestore()
-            .collection('users')
-            .doc(testUserId)
-            .delete()
+        it('Only authenticated user can UPDATE.', async () => {
+            const updateByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .doc(testUserTweetId)
+                .update({ text: "updated tweet." })
 
-        await testing.assertFails(deleteByAuthenticatedUser);
+            await testing.assertSucceeds(updateByAuthenticatedUser);
 
 
-        const deleteByUnauthenticatedUser = unauthenticatedUser.firestore()
-            .collection('users')
-            .doc(testUserId)
-            .delete()
+            const updateByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .doc(testUserTweetId)
+                .update({ text: "updated tweet." })
 
-        await testing.assertFails(deleteByUnauthenticatedUser);
+            await testing.assertFails(updateByUnauthenticatedUser);
+        });
+
+        it('Only authenticated user can DELETE.', async () => {
+            const deleteByAuthenticatedUser = authenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .doc(testUserTweetId)
+                .delete()
+
+            await testing.assertSucceeds(deleteByAuthenticatedUser);
+
+
+            const deleteByUnauthenticatedUser = unauthenticatedUser.firestore()
+                .collection('users')
+                .doc(testUserId)
+                .collection('tweets')
+                .doc(testUserTweetId)
+                .delete()
+
+            await testing.assertFails(deleteByUnauthenticatedUser);
+        });
     });
 });
